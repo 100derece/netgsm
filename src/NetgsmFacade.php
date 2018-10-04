ad623c5475c01413626500cd0e8279c6bc687d5d
Emre DEĞER <emredeger16@gmail.com>
2018-10-05 02:30:11 +0300

    fixed namespaces

--- src/NetgsmFacade.php
+++ src/NetgsmFacade.php
@@ -1,15 +1,8 @@
 <?php
 
-namespace Vomsis\Netgsm;
+namespace Yuzderece\Netgsm;
 
 use Illuminate\Support\Facades\Facade;
-
-/**
- * Created by PhpStorm.
- * User: macbookpro
- * Date: 18.02.2018
- * Time: 14:14
- */
 
 class NetgsmFacade extends Facade
 {
--- src/NetgsmServiceProvider.php
+++ src/NetgsmServiceProvider.php
@@ -1,33 +1,21 @@
 <?php
 
-namespace Vomsis\Netgsm;
+namespace Yuzderece\Netgsm;
 
 use Illuminate\Support\ServiceProvider;
-
-/**
- * Created by PhpStorm.
- * User: macbookpro
- * Date: 18.02.2018
- * Time: 13:59
- */
 
 class NetgsmServiceProvider extends ServiceProvider
 {
     public function boot(){
-        //ilk yüklendiğinde erişilir
-        //tanımlamalar yapılır
-
-        //paket config dosyasını uygulama içerisine aktarır
         $this->publishes([
             __DIR__.'/../config/netgsm.php'=>config_path('netgsm.php')
         ],'config'); //php artisan vendor:publish --tag='config'
     }
 
     public function register(){
-        //kayıt işlemleri
-        $this->mergeConfigFrom(__DIR__.'/../config/netgsm.php','netgsm');   //developer configleri ile default config dosyasını birleştirir
-        $this->app->singleton('netgsm',function ($app){ //ana uygulamaya netgsm isminde bir uygulama ekle(singleton - tekil olmasını sayğlıyor) netgsm uygulaması istendiğinde bu function çalışır.
-            $config=$app->make('config'); //laravel config uygulamasını getirir
+		$this->mergeConfigFrom(__DIR__.'/../config/netgsm.php','netgsm');
+		$this->app->singleton('netgsm',function ($app){
+            $config=$app->make('config');
             return new Netgsm($config);
         });
     }
